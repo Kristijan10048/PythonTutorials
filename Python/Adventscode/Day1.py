866486e018364296b06d0f33e023a4091545392a
Kris10048 <kristijan.petkov@gmail.com>
2021-01-09 18:09:03 +0100

    Added test file

---
+++ Python/Adventscode/Day1.py
@@ -0,0 +1,10 @@
+# Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+
+# For example, suppose your expense report contained the following:
+
+# 1721
+# 979
+# 366
+# 299
+# 675
+# 1456
