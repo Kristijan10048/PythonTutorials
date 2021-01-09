a91c23901ff6ab033fb85e146119b79e18e20707
Kris10048 <kristijan.petkov@gmail.com>
2021-01-09 18:27:43 +0100

    Initial Implementation of the alog

--- Python/Adventscode/Day1.py
+++ Python/Adventscode/Day1.py
@@ -1,10 +1,21 @@
-# Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+# https://adventofcode.com/2020/day/1
 
+# Specifically, they need you to find the two entries that sum to 2020 and 
+# then multiply those two numbers together.
 # For example, suppose your expense report contained the following:
-
 # 1721
 # 979
 # 366
 # 299
 # 675
 # 1456
+# In this list, the two entries that sum to 2020 are 1721 and 299. 
+# Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579
+
+myData = [ 1721,  979, 366, 299, 675, 1456]
+
+for i in myData:
+    print("Curr val:", i);
+    currValue = (2020 - i);
+    if currValue in myData :
+        print("Value ", currValue, " + ", i, " = ", i + currValue, "and the answer is:", i * currValue);
